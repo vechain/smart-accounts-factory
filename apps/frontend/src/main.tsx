35c396202dd3980cf0058cc474dabf5198810229
Dan Rusnac <rusnac14@gmail.com>
2025-01-24 15:21:26 +0100

    update kit

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { VeChainKit } from "@vechain/vechain-kit";
+import { VeChainKitProvider } from "@vechain/vechain-kit";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./utils/queryClient.ts";
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
-        <VeChainKit
+        <VeChainKitProvider
           feeDelegation={{
             delegatorUrl: import.meta.env.VITE_DELEGATOR_URL!,
             delegateAllTransactions: false,
@@ -33,7 +33,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           }}
         >
           <App />
-        </VeChainKit>
+        </VeChainKitProvider>
       </ChakraProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
